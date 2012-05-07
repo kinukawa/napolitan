e64a5ac884532308d2f876c3f30766a364346b11
Kenji Kinukawa <k.kinukawa@gmail.com>
2012-05-07 23:32:48 +0900

    POST用のエンドポイントを作った

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/upload', routes.getUploadPage);
+app.post('/upload', routes.postUploadData);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 
 /*
- * GET home page.
- */
+* GET home page.
+*/
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+    res.render('index', { title: 'Express' })
+};
+
+exports.getUploadPage = function(req, res){
+    res.render('upload', { title: 'Upload Page' })
+};
+
+exports.postUploadData = function(req, res){
+    res.redirect('/upload');
 };
